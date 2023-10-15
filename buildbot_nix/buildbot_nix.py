14556fd91f037ec4a676b00e873ddd572dc16ffc
Jörg Thalheim <joerg@thalheim.io>
2023-10-15 11:26:46 +0200

    disable periodic flake updates for now

--- buildbot_nix/buildbot_nix.py
+++ buildbot_nix/buildbot_nix.py
@@ -574,10 +574,10 @@ def config_for_project(
     nix_supported_systems: list[str],
     nix_eval_max_memory_size: int,
 ) -> Project:
-    # get a deterministic jitter for the project
-    random.seed(project.name)
-    # don't run all projects at the same time
-    jitter = random.randint(1, 60) * 60
+    ## get a deterministic jitter for the project
+    #random.seed(project.name)
+    ## don't run all projects at the same time
+    #jitter = random.randint(1, 60) * 60
 
     config["projects"].append(Project(project.name))
     config["schedulers"].extend(
@@ -624,11 +624,11 @@ def config_for_project(
                 buttonName="Update flakes",
             ),
             # updates flakes once a week
-            schedulers.Periodic(
-                name=f"{project.id}-update-flake-weekly",
-                builderNames=[f"{project.name}/update-flake"],
-                periodicBuildTimer=24 * 60 * 60 * 7 + jitter,
-            ),
+            #schedulers.Periodic(
+            #    name=f"{project.id}-update-flake-weekly",
+            #    builderNames=[f"{project.name}/update-flake"],
+            #    periodicBuildTimer=24 * 60 * 60 * 7 + jitter,
+            #),
         ]
     )
     has_cachix_auth_token = os.path.isfile(
