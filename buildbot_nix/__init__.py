9f2ec133e11463d0f71d89856f34a56829091d5f
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-10-27 11:09:20 +0200

    use rstrip to remove trailing newlines

--- buildbot_nix/__init__.py
+++ buildbot_nix/__init__.py
@@ -548,7 +548,7 @@ def read_secret_file(secret_name: str) -> str:
     if directory is None:
         print("directory not set", file=sys.stderr)
         sys.exit(1)
-    return Path(directory).joinpath(secret_name).read_text().strip()
+    return Path(directory).joinpath(secret_name).read_text().rstrip()
 
 
 @dataclass
