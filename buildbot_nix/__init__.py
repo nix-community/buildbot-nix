c477a14d1a230874aae1c8cf572706e382f979f9
Jörg Thalheim <joerg@thalheim.io>
2023-11-12 06:47:13 +0100

    fix github avatar authentication

--- buildbot_nix/__init__.py
+++ buildbot_nix/__init__.py
@@ -816,7 +816,7 @@ class NixConfigurator(ConfiguratorBase):
         if not config["www"].get("auth"):
             config["www"]["avatar_methods"] = config["www"].get("avatar_methods", [])
             config["www"]["avatar_methods"].append(
-                util.AvatarGitHub(self.github.token())
+                util.AvatarGitHub(token=self.github.token())
             )
             config["www"]["auth"] = util.GitHubAuth(
                 self.github.oauth_id, read_secret_file(self.github.oauth_secret_name)
