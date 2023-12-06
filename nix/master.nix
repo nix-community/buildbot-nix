642c1c2f12bd8b54229186e0df3f20933d67aa77
Jörg Thalheim <joerg@thalheim.io>
2023-12-06 09:00:14 +0100

    drop buildbot secret patch

--- nix/master.nix
+++ nix/master.nix
@@ -154,9 +154,6 @@ in
         in
         "${if hasSSL then "https" else "http"}://${cfg.domain}/";
       dbUrl = config.services.buildbot-nix.master.dbUrl;
-      package = (pkgs.buildbot.overrideAttrs (old: {
-        patches = old.patches ++ [ ./0001-allow-secrets-to-be-group-readable.patch ];
-      }));
       pythonPackages = ps: [
         ps.requests
         ps.treq
