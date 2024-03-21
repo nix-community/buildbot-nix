cd977bb3784c5e4909b522f5ea09eabb07aa00a9
Jörg Thalheim <joerg@thalheim.io>
2024-03-21 10:50:37 +0100

    re-introduce buildbot patch for old buildbot versions

--- nix/master.nix
+++ nix/master.nix
@@ -178,7 +178,8 @@ in
         in
         "${if hasSSL then "https" else "http"}://${cfg.domain}/";
       dbUrl = config.services.buildbot-nix.master.dbUrl;
-      package = (pkgs.buildbot.overrideAttrs (old: {
+      # Can be dropped after we have 24.05 everywhere
+      package = lib.mkIf (lib.versionOlder pkgs.buildbot.version "3.10.0") (pkgs.buildbot.overrideAttrs (old: {
         patches = old.patches ++ [ ./0001-allow-secrets-to-be-group-readable.patch ];
       }));
       pythonPackages = ps: [
