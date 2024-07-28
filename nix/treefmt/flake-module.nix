41ab4373ee2928f52ed94748c2ebb8138be3b726
Jörg Thalheim <joerg@thalheim.io>
2024-07-28 17:56:46 +0200

    use LICENSE.md for project root detection in flake-module.nix
    this fixes the project with juju or other git alternatives.

--- nix/treefmt/flake-module.nix
+++ nix/treefmt/flake-module.nix
@@ -5,7 +5,7 @@
     { pkgs, ... }:
     {
       treefmt = {
-        projectRootFile = ".git/config";
+        projectRootFile = "LICENSE.md";
         programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
         programs.nixfmt.package = pkgs.nixfmt-rfc-style;
         programs.shellcheck.enable = true;
