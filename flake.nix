dcf033c22408f85392c84b04fca0a61205818ca9
Jörg Thalheim <joerg@thalheim.io>
2024-10-05 14:15:31 +0200

    ci: speed up parallel eval

--- flake.nix
+++ flake.nix
@@ -87,7 +87,7 @@
               let
                 nixosMachines = lib.mapAttrs' (
                   name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel
-                ) ((lib.filterAttrs (_: config: config.pkgs.system == system)) self.nixosConfigurations);
+                ) ((lib.filterAttrs (name: _: lib.hasSuffix system name)) self.nixosConfigurations);
                 packages = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
                 devShells = lib.mapAttrs' (n: lib.nameValuePair "devShell-${n}") self'.devShells;
               in
