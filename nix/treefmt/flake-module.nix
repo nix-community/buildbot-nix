dff4ee94e6fa3fb7fa4f55801bd09a8589e7cbc8
magic_rb <richard@brezak.sk>
2024-11-09 21:16:42 +0100

    Actually enable `mypy` in `treefmt`
    Signed-off-by: magic_rb <richard@brezak.sk>

--- nix/treefmt/flake-module.nix
+++ nix/treefmt/flake-module.nix
@@ -19,13 +19,25 @@
 
         programs.mypy = {
           enable = pkgs.stdenv.buildPlatform.isLinux;
-          directories.".".extraPythonPackages = [
-            pkgs.buildbot
-            pkgs.buildbot-worker
-            pkgs.python3.pkgs.twisted
-          ];
+          package = pkgs.buildbot.python.pkgs.mypy;
+          directories."." = {
+            modules = [
+              "buildbot_nix"
+            ];
+            extraPythonPackages = [
+              (pkgs.python3.pkgs.toPythonModule pkgs.buildbot)
+              pkgs.buildbot-worker
+              pkgs.python3.pkgs.twisted
+              pkgs.python3.pkgs.pydantic
+              pkgs.python3.pkgs.zope-interface
+            ];
+          };
         };
 
+        # the mypy module adds `./buildbot_nix/**/*.py` which does not appear to work
+        # furthermore, saying `directories.""` will lead to `/buildbot_nix/**/*.py` which
+        # is obviously incorrect...
+        settings.formatter."mypy-.".includes = [ "buildbot_nix/**/*.py" ];
         settings.formatter.ruff-check.priority = 1;
         settings.formatter.ruff-format.priority = 2;
       };
