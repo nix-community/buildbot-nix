0ed3236417d5a53402159c58341bab3d688d8401
zowoq <59103226+zowoq@users.noreply.github.com>
2025-04-15 09:16:42 +1000

    add skipBuildIf patch

--- nix/packages.nix
+++ nix/packages.nix
@@ -18,7 +18,15 @@ in
 
     buildbot = lib.mkOption {
       type = lib.types.package;
-      default = pkgs.buildbot;
+      default = pkgs.buildbot.overrideAttrs (o: {
+        patches = o.patches ++ [
+          (pkgs.fetchpatch {
+            name = "add-Build.skipBuildIf.patch";
+            url = "https://github.com/buildbot/buildbot/commit/f08eeef96e15c686a4f6ad52368ad08246314751.patch";
+            hash = "sha256-ACPYXMbjIfw02gsKwmDKIIZkGSxxLWCaW7ceEcgbtIU=";
+          })
+        ];
+      });
     };
 
     buildbot-worker = lib.mkOption {
