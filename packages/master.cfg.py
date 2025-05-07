c771fcbbd51d6b74834ae4151428b86f2b7191a2
Jörg Thalheim <joerg@thalheim.io>
2025-05-07 15:52:08 +0200

    dev-workflow: config gcroots_dir

--- packages/master.cfg.py
+++ packages/master.cfg.py
@@ -3,6 +3,7 @@ from buildbot_nix import (
     NixConfigurator,
     BuildbotNixConfig,
 )
+import getpass
 from buildbot.plugins import util
 from buildbot.process.factory import BuildFactory
 
@@ -11,6 +12,14 @@ factory = BuildFactory()
 STATE_DIR = Path(".")
 PORT = 8012
 url = f"http://localhost:{PORT}"
+current_user = getpass.getuser()
+gcroots_dir = Path("/nix/var/nix/gcroots/per-user/") / current_user
+if not gcroots_dir.exists():
+    raise RuntimeError(
+        f"GC roots directory {gcroots_dir} does not exist. "
+        "Please ensure that the Nix daemon is running and that you have the correct permissions."
+        f" You can create it with `sudo mkdir -p {gcroots_dir}` and `sudo chown {current_user} {gcroots_dir}`."
+    )
 
 buildbot_nix_config = BuildbotNixConfig(
     db_url="sqlite:///state.sqlite",
@@ -31,6 +40,7 @@ buildbot_nix_config = BuildbotNixConfig(
     domain="localhost",
     webhook_base_url=url,
     url=url,
+    gcroots_dir=gcroots_dir,
     admins=["admin"],
 )
 
